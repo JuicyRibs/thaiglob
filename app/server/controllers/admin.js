4a134be3466cb13e812b26fa1d7facf9fa1e6f39
Thanadon Teeratadapong <thanadon.rh@mail.kmutt.ac.th>
2020-07-05 02:49:18 +0700

    GA kindof works Edit-Delete Part 1/?

--- app/server/controllers/admin.js
+++ app/server/controllers/admin.js
@@ -1,9 +1,21 @@
+const Article = require('../models/article');
+
 exports.getIndex = (req, res) => {
 	res.render('admin/index');
 };
 
 exports.getArticle = (req, res) => {
 	res.render('admin/create/article');
+};
+exports.editArticle = (req, res) => {
+	Article.findById(req.params.id, function (err, article) {
+		if (err) {
+			return next(err);
+		}
+		res.render('admin/edit/article', {
+			data: article,
+		});
+	});
 };
 
 exports.getEvent = (req, res) => {
--- app/server/routes/admin.js
+++ app/server/routes/admin.js
@@ -2,25 +2,37 @@ const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/admin');
 const authMiddleware = require('../middlewares/auth');
-
+// INDEX
 router.get('/', authMiddleware.isAuthenticated, adminController.getIndex);
+
+// ARTICLE
 router.get(
 	'/article',
 	authMiddleware.isAuthenticated,
 	adminController.getArticle
 );
+router.get(
+	'/article/:id/edit',
+	authMiddleware.isAuthenticated,
+	adminController.editArticle
+);
+// EVENT
 router.get('/event', authMiddleware.isAuthenticated, adminController.getEvent);
+// BOOKS
 router.get(
 	'/book',
 	authMiddleware.isAuthenticated,
 	adminController.getPublication
 );
+// MEDIA
 router.get(
 	'/media',
 	authMiddleware.isAuthenticated,
 	adminController.getMultimedia
 );
+// NEWS
 router.get('/news', authMiddleware.isAuthenticated, adminController.getNews);
+// RESEARCH
 router.get(
 	'/research',
 	authMiddleware.isAuthenticated,
